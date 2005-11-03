0d3c0c54730c87ab23172a9e54d24842f2dee36a
floam <floam@edf5b092-35ff-0310-97b2-ce42778d08ea>
2005-11-03 03:46:09 +0000

    HTML tables are supposed to be two-dimensional, I guess.
    git-svn-id: svn://svn.icculus.org/quake3/trunk@243 edf5b092-35ff-0310-97b2-ce42778d08ea

--- web/include/status.php
+++ web/include/status.php
@@ -1,5 +1,5 @@
 <h2>Port Status</h2>
-<p>Here is a table of the most up to date information we have regarding icculus.org/Quake3's working status on different platforms. There are things to bear in mind when viewing this:
+<p>Here is a table of the most up to date information we have regarding icculus.org/Quake3's working status on different operating systems. There are things to bear in mind when viewing this:
 </p>
 <ul>
 	<li>The status column refers only to the latest information we are given, so it may be out of date.</li>
@@ -7,80 +7,81 @@
 	<li>Status is one of <em>working</em>, <em>broken</em> or <em>unknown</em>.</li>
 </ul>
 <table>
+	<caption>Working Status of Port Targets</caption>
 	<tr>
-		<th>Platform</th>
-		<th>Architecture</th>
-		<th>Status</th>
-		<th><acronym title="Last Known Working Revision">LKWR</acronym></th>
-		<th>Notes</th>
+		<th scope="col">OS</th>
+		<th scope="col">Platform</th>
+		<th scope="col">Status</th>
+		<th scope="col"><acronym title="Last Known Working Revision">LKWR</acronym></th>
+		<th scope="col">Notes</th>
 	</tr>
 	<tr>
-		<th rowspan="3">Linux</th> <!--Platform-->
-		<td>gcc-x86</td> <!--Arch-->
+		<td scope="row" rowspan="3">Linux</td> <!--OS-->
+		<td>gcc-x86</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	<tr>
-		<td>gcc-x86_64</td> <!--Arch-->
+		<td>gcc-x86_64</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td>x86_64 JIT bytecode compiler disabled by default.</td> <!--Notes-->
 	</tr>
 	<tr>
-		<td>gcc-ppc</td> <!--Arch-->
+		<td>gcc-ppc</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	<tr>
-		<th rowspan="2">Windows</th> <!--Platform-->
-		<td>mingw-x86</td> <!--Arch-->
+		<td scope="row" rowspan="2">Windows</td> <!--OS-->
+		<td>mingw-x86</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	<tr>
-		<td>MSVC-x86</td> <!--Arch-->
+		<td>MSVC-x86</td> <!--Platform-->
 		<td>unknown</td> <!--Status-->
 		<td><a href="http://svn.icculus.org/quake3/trunk/?rev=180">180</a>ish?</td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	<tr>
-		<th rowspan="2">Max OSX</th> <!--Platform-->
-		<td>xcode-G3</td> <!--Arch-->
+		<td scope="row" rowspan="2">Max OSX</td> <!--OS-->
+		<td>xcode-G3</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	<tr>
-		<td>xcode-G4</td> <!--Arch-->
+		<td>xcode-G4</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td>Client has severe graphical glitches.</td> <!--Notes-->
 	</tr>
-		<th>FreeBSD</th> <!--Platform-->
-		<td>gcc-x86</td> <!--Arch-->
+		<td scope="row">FreeBSD</td> <!--OS-->
+		<td>gcc-x86</td> <!--Platform-->
 		<td>working</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	</tr>
-		<th rowspan="2">Solaris</th> <!--Platform-->
-		<td>gcc-x86</td> <!--Arch-->
+		<td scope="row" rowspan="2">Solaris</td> <!--OS-->
+		<td>gcc-x86</td> <!--Platform-->
 		<td>broken</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	</tr>
-		<td>gcc-sparc</td> <!--Arch-->
+		<td>gcc-sparc</td> <!--Platform-->
 		<td>broken</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td></td> <!--Notes-->
 	</tr>
 	</tr>
-		<th>IRIX</th> <!--Platform-->
-		<td>cc-*</td> <!--Arch-->
+		<td scope="row">IRIX</td> <!--OS-->
+		<td>cc-*</td> <!--Platform-->
 		<td>broken</td> <!--Status-->
 		<td></td> <!--LKWR-->
 		<td>Doesn't exist. Should we drop it?</td> <!--Notes-->
