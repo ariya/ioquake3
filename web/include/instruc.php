4fcb2180428ac96b603b888b21c54412dc9db4e8
floam <floam@edf5b092-35ff-0310-97b2-ce42778d08ea>
2005-11-03 06:38:00 +0000

    More minor stuff
    git-svn-id: svn://svn.icculus.org/quake3/trunk@247 edf5b092-35ff-0310-97b2-ce42778d08ea

--- web/include/instruc.php
+++ web/include/instruc.php
@@ -1,11 +1,34 @@
-<h2>How to check out and build:</h2>
+<h2>Check out and build from Subversion</h2>
 <ol>
-	<li>Step 1</li>
-	<li>Step 2</li>
-	<li>Step 3</li>
-	<li>Step 4</li>
-	<li>Step 5</li>
-	<li>Step etc</li>
+	<li>Change into a directory that you want the tree to be kept in.</li>
+	<li>Check out the source tree:<br />
+	<span class="shell">svn co svn://svn.icculus.org/quake3/trunk quake3</span>
+	<br />
+	(You can check out a specific revision with the -r option, like this:
+	<span class="shell">svn co svn://svn.icculus.org/quake3/trunk quake3 -r100</span>)
+	</li>
+	<li>Read the <a href="http://svn.icculus.org/*checkout*/quake3/trunk/i_o-q3-readme">readme</a> file. Really. Do it.</li>
+	<li>Compile and install Quake III.
+		<ul>
+			<li><h3>Linux and friends</h3>
+				<ol>
+					<li>Change into the top level directory (it contains the
+					<span class="shell">ui</span> and <span class="shell">code</span>
+					directories.)</li>
+					<li>Run <span class="shell">make</span></li>
+					<li>Create a directory to install Quake3 into:<br />
+					<span class="shell">mkdir /usr/local/games/quake3</span></li>
+					<li>Copy 
+					
+				</ol>
+			</li>
+			<li><h3>Windows</h3>
+				<ol>
+					<li></li>
+				</ol>
+			</li>
+		</ul>
+	</li>
 </ol>
 <h2>How to submit your changes:</h2>
 <p>rules rules rules, angry zakk-style text rules rules rules, angry zakk-style text rules rules rules, angry zakk-style text rules rules rules, angry zakk-style text rules rules rules, angry zakk-style text rules rules rules, angry zakk-style textrules rules rules, angry zakk-style text</p>
