398fce484c28ebc7e6c92d17143c094d74a01c14
floam <floam@edf5b092-35ff-0310-97b2-ce42778d08ea>
2005-11-03 03:07:55 +0000

    Status page, minor updates.
    git-svn-id: svn://svn.icculus.org/quake3/trunk@240 edf5b092-35ff-0310-97b2-ce42778d08ea

--- web/include/home.php
+++ web/include/home.php
@@ -1,10 +1,9 @@
-<p>Quake 3 now runs natively on many platforms thanks to id Software for releasing the engine 
-source code.</p>
+<p>Quake 3 source code was released on August 20th by the fine folks at id Software. We thank them for that, and are continuing the mission of a Quake 3 that is without fault.</p>
 <img src="images/quake3.jpg" class="right" />
 <h2>What's the point?</h2>
 <p>This project aims to build upon id Software's Quake 3 source code release.
 The source was released on August 20, 2005 under the GPL license. Since then, we have
-feverishly been cleaning up, fixing bugs of, and adding features to, Quake 3. Our eventual
+feverishly been cleaning up, fixing bugs of, and adding features to it. Our eventual
 goal is to have created <strong>the</strong> Quake3 source code distribution that people
 base their games and mods with their own pointy goals upon. Our focus is initially
 to get the game working with our updates on Mac OS X, Windows, and Linux. Sane new features are also welcome. Modern graphical upgrades (bloom lighting, etc) would have to be disabled by dedfault
@@ -12,7 +11,7 @@ to get the game working with our updates on Mac OS X, Windows, and Linux. Sane n
 <h2>Progress.</h2>
 <p>While a lot is on the horizon, quite a few goals have been completed already. Quake 3
 now works natively on x86_64 and PowerPC architectures, and the game builds and runs
-in FreeBSD. SDL is now used for input/context/sound, making the game far easier to port
+in FreeBSD. SDL is now used for input / contexts / sound, making the game far easier to port
 to new platforms and architectures. Many security holes and other problems have been repaired. A more in-depth database
 of new features, working status of platforms, and bugs is the <a href="?page=status">Status</a>
 section.</p>
--- web/include/status.php
+++ web/include/status.php
@@ -1,2 +1,89 @@
-<h2>tba</h2>
-<p>asdasd</p>
+<h2>Port Status</h2>
+<p>Here is a table of the most up to date information we have regarding icculus.org/Quake3's working status on different platforms. There are things to bear in mind when viewing this:
+</p>
+<ul>
+	<li>The status column refers only to the latest information we are given, so it may be out of date.</li>
+	<li>"Working" doesn't always imply "usable", see the <em>Notes</em> column.</li>
+	<li>Status is one of <em>working</em>, <em>broken</em> or <em>unknown</em>.</li>
+</ul>
+<table>
+	<tr>
+		<th>Platform</th>
+		<th>Architecture</th>
+		<th>Status</th>
+		<th><acronym title="Last Known Working Revision">LKWR</acronym></th>
+		<th>Notes</th>
+	</tr>
+	<tr>
+		<th rowspan="3">Linux</th> <!--Platform-->
+		<td>gcc-x86</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	<tr>
+		<td>gcc-x86_64</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td>x86_64 JIT bytecode compiler disabled by default.</td> <!--Notes-->
+	</tr>
+	<tr>
+		<td>gcc-ppc</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	<tr>
+		<th rowspan="2">Windows</th> <!--Platform-->
+		<td>mingw-x86</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	<tr>
+		<td>MSVC-x86</td> <!--Arch-->
+		<td>unknown</td> <!--Status-->
+		<td><a href="http://svn.icculus.org/quake3/trunk/?rev=180">180</a>ish?</td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	<tr>
+		<th rowspan="2">Max OSX</th> <!--Platform-->
+		<td>xcode-G3</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	<tr>
+		<td>xcode-G4</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td>Client has severe graphical glitches.</td> <!--Notes-->
+	</tr>
+		<th>FreeBSD</th> <!--Platform-->
+		<td>gcc-x86</td> <!--Arch-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	</tr>
+		<th rowspan="2">Solaris</th> <!--Platform-->
+		<td>gcc-x86</td> <!--Arch-->
+		<td>broken</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	</tr>
+		<td>gcc-sparc</td> <!--Arch-->
+		<td>broken</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
+	</tr>
+	</tr>
+		<th>IRIX</th> <!--Platform-->
+		<td>cc-*</td> <!--Arch-->
+		<td>broken</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td>Doesn't exist. Should we drop it?</td> <!--Notes-->
+	</tr>
+</table>
+<h2>Implemented Features</h2>
