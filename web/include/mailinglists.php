786234f6f2ea42a981c3a7d01d153e58faa2af4a
floam <floam@edf5b092-35ff-0310-97b2-ce42778d08ea>
2005-11-11 19:27:26 +0000

    Forgot to svn add it.
    git-svn-id: svn://svn.icculus.org/quake3/trunk@326 edf5b092-35ff-0310-97b2-ce42778d08ea

---
+++ web/include/mailinglists.php
@@ -0,0 +1,27 @@
+<h2>Mailing List Information</h2>
+<p>There are two mailing lists used by this project. Here's a description and instructions
+for each.</p>
+<h3 class="shell">quake3@icculus.org</h3>
+<p>There is a general discussion mailing list where you can discuss this project,
+ask for help, announce patches, and anything else that could possibly be talked
+about with the developers and users of icculus.org/quake3.</p>
+<p>To join the list, send a blank e-mail to
+<a href="mailto:quake3-subscribe@icculus.org" class="shell">quake3-subscribe@icculus.org</a>.
+<br />
+To send an e-mail to the list, address it to
+<a href="mailto:quake3@icculus.org" class="shell">quake3@icculus.org</a>.
+<br />
+To unsubscribe, send a blank e-mail to
+<a href="mailto:quake3-unsubscribe@icculus.org" class="shell">quake3-unsubscribe@icculus.org</a>.
+<br />
+For more help, send a blank email to
+<a href="mailto:quake3-help@icculus.org" class="shell">quake3-help@icculus.org</a>.
+All of the above information plus more will be sent to you.
+</p>
+<h3 class="shell">quake3-commits@icculus.org</h3>
+<p>There is also a read-only mailing list which tracks changes to the
+<acronym title="Subversion">SVN</acronym> tree. You'll be sent an email each time
+someone commits a change. This is not normally a high-volume list, but it certainly
+can be at times. The instructions for this list are identical to the instructions
+for regular list, just change the <span class="shell">quake3</span> part of the
+e-mail addresses to <span class="shell">quake3-commits</span>.</p>
