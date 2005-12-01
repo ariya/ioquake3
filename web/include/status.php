e88d9dd4440391afba90e41b93b5438063e124b2
tma <tma@edf5b092-35ff-0310-97b2-ce42778d08ea>
2005-12-01 21:32:01 +0000

    * Update MinGW status
    git-svn-id: svn://svn.icculus.org/quake3/trunk@403 edf5b092-35ff-0310-97b2-ce42778d08ea

--- web/include/status.php
+++ web/include/status.php
@@ -43,9 +43,9 @@
 	<tr>
 		<td scope="row" rowspan="2">Windows</td> <!--OS-->
 		<td>mingw-x86</td> <!--Platform-->
-		<td>broken</td> <!--Status-->
-		<td><a href="http://svn.icculus.org/quake3/trunk/?rev=372">372</a></td> <!--LKWR-->
-		<td>Included OpenAL headers are too old right now.</td> <!--Notes-->
+		<td>working</td> <!--Status-->
+		<td></td> <!--LKWR-->
+		<td></td> <!--Notes-->
 	</tr>
 	<tr>
 		<td>MSVC-x86</td> <!--Platform-->
