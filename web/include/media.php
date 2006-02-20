8e2e7055482d04d3941cc0f803bd01324521f08f
floam <floam@edf5b092-35ff-0310-97b2-ce42778d08ea>
2006-02-20 09:14:51 +0000

    General outline of upcoming media page.
    git-svn-id: svn://svn.icculus.org/quake3/trunk@558 edf5b092-35ff-0310-97b2-ce42778d08ea

---
+++ web/include/media.php
@@ -0,0 +1,27 @@
+<h2>Screenshots</h2>
+<p>Here be some screenshots of icculus.org in action on different platforms and games.</p>
+<ul>
+<li>
+	<h3>Quake 3 Arena</h3>
+	<li>
+		On Linux/x86_64:<br />
+		<img src="/images/screenshots/arenalinux.png" />
+	</li>
+	<li>
+		On Mac OS X:<br />
+		<img src="/images/screenshots/arenaosx.png" />
+	</li>
+</li>
+<li>
+	<h3>Tremulous<h3>
+	<p><a href="http://tremulous.net/">Tremulous</a> is the first Quake3 mod to evolve
+	into it's very own standalone game, based upon our engine.</p>
+	<li>
+		On Linux/x86:<br />
+		<img src="/images/screenshots/tremlinux.png" />
+	</li>
+
+
+<h2>Logo</h2>
+<p>Put some information here like most places do with links to big versions of the
+logo and talk about general asshattery.</p>
