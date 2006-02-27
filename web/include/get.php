990e838d26d47d3ae83a36c32fab6e66038b61f9
floam <floam@edf5b092-35ff-0310-97b2-ce42778d08ea>
2006-02-27 03:34:19 +0000

    link context
    git-svn-id: svn://svn.icculus.org/quake3/trunk@592 edf5b092-35ff-0310-97b2-ce42778d08ea

--- web/include/get.php
+++ web/include/get.php
@@ -17,7 +17,8 @@ done all of the work for you.</p>
 <ul>
 <li>
 		<h3>Windows</h3>
-		<p>There is a Windows installer by <a href="mailto:phatfil@optusnet.com.au">Philip Nelson</a> <a href="http://www.timedoctor.org/~zakk/ioquake3_setup.exe">here</a>.</p>
+		<p>There is a <a href="http://www.timedoctor.org/~zakk/ioquake3_setup.exe">
+		Windows installer</a> by <a href="mailto:phatfil@optusnet.com.au">Philip Nelson</a>.</p>
 </li>
 <li>
 		<h3>Debian &amp; Ubuntu</h3>
