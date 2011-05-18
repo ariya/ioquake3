ariya/ioquake3

commit f608a6eb19407728b97b96d6793db0e0c8b6517a

- Use edx for CALLs instead of ecx, as the latter does not require any push to stack - Make Linux AT&T asm code consistent with the MASM codeblock
git-svn-id: svn://svn.icculus.org/quake3/trunk@1994 edf5b092-35ff-0310-97b2-ce42778d08ea

code/qcommon/vm_x86.c
@@ -329,7 +329,7 @@ qboolean EmitMovEBXEDI(vm_t *vm, int andit) {
 /*
 =================
 DoSyscall
-Uses asm to get arguments from stack to work around different calling conventions
+Uses asm to retrieve arguments from registers to work around different calling conventions
 =================
 */
 
@@ -342,7 +342,6 @@ static void DoSyscall(void)
 	int programStack;
 	int *opStack;
 	
-	// Get arguments directly from registers to work around different calling conventions
 #ifdef _MSC_VER
 	__asm
 	{
@@ -417,8 +416,8 @@ int EmitCallProcedure(vm_t *vm)
 	retval = compiledOfs;
 
 	EmitString("F7 D0");		// not eax
-	// use ecx register to store DoSyscall address
-	EmitString("B9");		// mov ecx, DoSyscall
+	// use edx register to store DoSyscall address
+	EmitString("BA");		// mov edx, DoSyscall
 	Emit4((intptr_t) DoSyscall);
 
 	// align the stack pointer to a 16-byte-boundary
@@ -427,7 +426,7 @@ int EmitCallProcedure(vm_t *vm)
 	EmitString("83 E4 F0");		// and esp, 0xFFFFFFF0
 			
 	// call the syscall wrapper function
-	EmitString("FF D1");		// call ecx
+	EmitString("FF D2");		// call edx
 
 	// reset the stack pointer to its previous value
 	EmitString("89 EC");		// mov esp, ebp
@@ -442,26 +441,24 @@ int EmitCallProcedure(vm_t *vm)
 
 void EmitCall(vm_t *vm, int sysCallOfs)
 {
-       	EmitString("51");	// push ecx
-       	EmitString("8B 0D");	// mov ecx, dword ptr [&vm->codeBase]
+       	EmitString("8B 15");	// mov edx, [dword ptr vm->codeBase]
        	Emit4((intptr_t) &vm->codeBase);
 
        	if(sysCallOfs)
        	{
 	       	if(sysCallOfs < 0x80 || sysCallOfs > 0x7f)
 	       	{
-		       	EmitString("83 C1");	// add ecx, sysCallOfs
+		       	EmitString("83 C2");	// add edx, sysCallOfs
 		       	Emit1(sysCallOfs);
 		}
 		else
 		{
-			EmitString("81 C1");	// add ecx, sysCallOfs
+			EmitString("81 C2");	// add edx, sysCallOfs
 			Emit4(sysCallOfs);
 		}
 	}
 
-	EmitString("FF D1");	// call ecx
-	EmitString("59");	// pop ecx
+	EmitString("FF D2");	// call edx
 }
 
 /*
@@ -1258,7 +1255,7 @@ void VM_Compile( vm_t *vm, vmHeader_t *header ) {
 			break;
 		case OP_DIVI:
 			EmitString( "8B 47 FC" );	// mov eax,dword ptr [edi-4]
-			EmitString( "99" );			// cdq
+			EmitString( "99" );		// cdq
 			EmitString( "F7 3F" );		// idiv dword ptr [edi]
 			EmitString( "89 47 FC" );	// mov dword ptr [edi-4],eax
 			EmitCommand(LAST_COMMAND_SUB_DI_4);		// sub edi, 4
@@ -1272,7 +1269,7 @@ void VM_Compile( vm_t *vm, vmHeader_t *header ) {
 			break;
 		case OP_MODI:
 			EmitString( "8B 47 FC" );	// mov eax,dword ptr [edi-4]
-			EmitString( "99" );			// cdq
+			EmitString( "99" );		// cdq
 			EmitString( "F7 3F" );		// idiv dword ptr [edi]
 			EmitString( "89 57 FC" );	// mov dword ptr [edi-4],edx
 			EmitCommand(LAST_COMMAND_SUB_DI_4);		// sub edi, 4
@@ -1546,18 +1543,16 @@ int	VM_CallCompiled( vm_t *vm, int *args ) {
 			popad
 		}
 #else
-		/* These registers are used as scratch registers and are destroyed after the
-		 * call.  Do not use clobber, so they can be used as input for the asm. */
-		unsigned eax;
-		unsigned ebx;
-		unsigned ecx;
-		unsigned edx;
-
 		__asm__ volatile(
-			"call *%6"
-			: "+S" (programStack), "+D" (opStack),
-			  "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
-			: "mr" (vm->codeBase + vm->entryOfs)
+		        "pushal\r\n"
+		        "movl %0, %%esi\r\n"
+		        "movl %1, %%edi\r\n"
+			"call *%2\r\n"
+			"movl %%edi, %1\r\n"
+			"movl %%esi, %0\r\n"
+			"popal\r\n"
+			: "+g" (programStack), "+g" (opStack)
+			: "g" (vm->codeBase + vm->entryOfs)
 			: "cc", "memory"
 		);
 #endif

commit 5ea1875e62593d1ff268d748c4437a0185c13ba2

Fix VM call for release version, bug introduced by myself in r1994
git-svn-id: svn://svn.icculus.org/quake3/trunk@1996 edf5b092-35ff-0310-97b2-ce42778d08ea

code/qcommon/vm_x86.c
@@ -1491,6 +1491,7 @@ This function is called directly by the generated code
 */
 int	VM_CallCompiled( vm_t *vm, int *args ) {
 	int		stack[1024];
+	void	*entryPoint;
 	int		programCounter;
 	int		programStack;
 	int		stackOnEntry;
@@ -1527,36 +1528,28 @@ int	VM_CallCompiled( vm_t *vm, int *args ) {
 	*(int *)&image[ programStack ] = -1;	// will terminate the loop on return
 
 	// off we go into generated code...
+	entryPoint = vm->codeBase + vm->entryOfs;
 	opStack = &stack;
 
-	{
 #ifdef _MSC_VER
-		void *entryPoint = vm->codeBase + vm->entryOfs;
-
-		__asm {
-			pushad
-			mov    esi, programStack
-			mov    edi, opStack
-			call   entryPoint
-			mov    programStack, esi
-			mov    opStack, edi
-			popad
-		}
-#else
-		__asm__ volatile(
-		        "pushal\r\n"
-		        "movl %0, %%esi\r\n"
-		        "movl %1, %%edi\r\n"
-			"call *%2\r\n"
-			"movl %%edi, %1\r\n"
-			"movl %%esi, %0\r\n"
-			"popal\r\n"
-			: "+g" (programStack), "+g" (opStack)
-			: "g" (vm->codeBase + vm->entryOfs)
-			: "cc", "memory"
-		);
-#endif
+	__asm
+	{
+		pushad
+		mov    esi, programStack
+		mov    edi, opStack
+		call   entryPoint
+		mov    programStack, esi
+		mov    opStack, edi
+		popad
 	}
+#else
+	__asm__ volatile(
+		"call *%2\r\n"
+		: "+S" (programStack), "+D" (opStack)
+		: "r" (vm->codeBase + vm->entryOfs)
+		: "cc", "memory", "%eax", "%ebx", "%ecx", "%edx"
+	);
+#endif
 
 	if ( opStack != &stack[1] ) {
 		Com_Error( ERR_DROP, "opStack corrupted in compiled code" );